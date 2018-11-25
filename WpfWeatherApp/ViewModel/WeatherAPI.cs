ecb4f31c3d0dcd7f6589a8b08255ce9197edba91
Justin Wyatt <justin.j.wyatt.ctr2@mail.mil>
2018-11-24 19:56:42 -0600

    Added model and view model. Imported JSON converter

---
+++ WpfWeatherApp/ViewModel/WeatherAPI.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WpfWeatherApp.Model;
+
+namespace WpfWeatherApp.ViewModel
+{
+    public class WeatherAPI
+    {
+        public const string API_KEY = "";
+        public const string BASE_URL = "";
+
+        public async Task<WeatherResult> GetWeatherInformationAsync(string city)
+        {
+            var result = new WeatherResult();
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(BASE_URL);
+
+                var json = await response.Content.ReadAsStringAsync();
+
+                result = JsonConvert.DeserializeObject<WeatherResult>(json);
+            }
+
+            return result;
+        }
+    }
+}

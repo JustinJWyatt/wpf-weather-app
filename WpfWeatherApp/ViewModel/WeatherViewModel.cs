ba5b5a1a7c8664b728320e4f25b6a65e27ba8981
Justin Wyatt <justin.j.wyatt.ctr2@mail.mil>
2018-11-25 01:54:44 -0600

    Binding ViewModel to UI

---
+++ WpfWeatherApp/ViewModel/WeatherViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfWeatherApp.Model;
+
+namespace WpfWeatherApp.ViewModel
+{
+    public class WeatherViewModel
+    {
+        public WeatherResult Weather { get; set; }
+    }
+}
